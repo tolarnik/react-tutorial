fcbac6c212bb5e9e5f4f44d21bdca97109d38931
Jiri Hauser <jirihauser94@gmail.com>
2019-05-08 19:50:11 +0100

    LEKCE 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Player from "./components/Player";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello world</h1>
+                <Player />
             </div>
         );
     }
---
+++ src/components/Player.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const styles = {
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    center: {
+        fontSize: '5rem'
+    }
+};
+
+class Player extends React.PureComponent {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+
+    increment = () => {
+        this.setState(prevState => ({counter: prevState.counter +1}))
+    };
+
+    decrement = () => {
+        this.setState(prevState => ({counter: prevState.counter -1}))
+    };
+
+    render() {
+        return (
+            <div style={styles.card}>
+                <button onClick={this.increment} data-cy="inc-1">
+                    <span role="img" aria-label="Plus">➕</span>
+                </button>
+                <div style={styles.center} data-cy="val-1">
+                    {this.state.counter}
+                </div>
+                <button onClick={this.decrement} data-cy="dec-1">
+                    <span role="img" aria-label="Minus">➖</span>
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Player
